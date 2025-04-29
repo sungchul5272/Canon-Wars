4ce4510bf29a48d6302a11673422e6adcb4d79c4
권영욱 <0duddnr0@naver.com>
2025-04-29 19:24:18 +0900

    영욱 - BGM, SFX 추가

--- Assets/2.Scripts/Contents/Map/MapData.cs
+++ Assets/2.Scripts/Contents/Map/MapData.cs
@@ -14,11 +14,17 @@ public enum eMapType
 [CreateAssetMenu(fileName = "MapData", menuName = "Scriptable Objects/MapData")]
 public class MapData : ScriptableObject
 {
+    [Header("Prefab")]
     public GameObject backgroundPrefab = null;
     public GameObject foregroundPrefab = null;
 
+    [Header("Info")]
     public eMapType mapType = eMapType.Max;
     public Vector2 mapSize = Vector2.zero;
 
+    [Header("Image")]
     public Sprite _mapSprite = null;
+
+    [Header("Sound")]
+    public AudioClip sound = null;
 }
